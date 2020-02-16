6bff8d3f16595f17af25b7df2b8516e379de7d0e
sander-aaron <31959117+sander-aaron@users.noreply.github.com>
2020-02-16 16:48:53 -0500

    Add js and btn

--- _patterns/buttons/btn.js
+++ _patterns/buttons/btn.js
@@ -12,3 +12,11 @@ $btn.on('click', function(e) {
   e.preventDefault();
   $('html, body').animate({scrollTop:0}, '300');
 });
+
+;$('.play-sound').click(function() {
+    if ($('video').get(0).muted == true) {
+        $('video').get(0).muted = false;
+    } else {
+        $('video').get(0).muted = true;
+    }
+});
